8973316e6bca6aded2282e8e6b9777c2c6a327e7
haywah27 <hayleywahlroos15@gmail.com>
2021-03-27 11:01:37 -0700

    poppulating color variable

---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Select from "react-select";
+
+function ColorChange(){
+    const colors = [
+        {
+            value: 1,
+            label: "blue"
+        },
+        {
+            value: 2,
+            label: "pink"
+        },
+        {
+            value: 3,
+            label: "green"
+        },
+        {
+            value: 4,
+            label: "purple"
+        },
+        {
+            value: 5,
+            label: "orange"
+        }
+    ];
+
+    return ();
+
+}
+
+export default ColorChange;
